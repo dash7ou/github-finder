40d6063a06a68c031d31a1d0bd0996ec8cf87abb
DasH <fxfddfno@gmail.com>
2020-02-03 15:07:14 +0200

    deploy app

--- src/context/gihub/GithubState.js
+++ src/context/gihub/GithubState.js
@@ -12,7 +12,16 @@ import {
     GET_USER
 } from "../type";
 
+let githubClientId; 
+let githubClientSecret;
 
+if(process.env.NODE_ENV !== 'production'){
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret= process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props =>{
     const initialState = {
@@ -28,8 +37,7 @@ const GithubState = props =>{
 		setDataLoading(true);
 		try {
 			const res = await axios.get(
-				`https://api.github.com/search/users?q=${text}&client_id=${process.env
-					.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+				`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 			);
 			if (res.data.items.length === 0) {
 				clearUsers()
@@ -49,8 +57,7 @@ const GithubState = props =>{
 		setDataLoading(true);
 		try {
 			const res = await axios.get(
-				`https://api.github.com/users/${username}?client_id=${process.env
-					.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+				`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
 			);
 			setDataLoading(false);
 			dispatch({
@@ -72,8 +79,7 @@ const GithubState = props =>{
 		setDataLoading(true);
 		try {
 			const res = await axios.get(
-				`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env
-					.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+				`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 			);
 			setDataLoading(false);
 			dispatch({
